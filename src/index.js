f7522cf9143762c51a8dd29c0e3928fc1d70f5f1
Jonathan <jadit2@gmail.com>
2016-02-01 17:24:52 -0800

    Support browser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports = {
+var api = {
     // core src/views
     Text: require('./views/text'),
     Table: require('./views/table'),
@@ -13,3 +13,24 @@ module.exports = {
     // prototypes
     TimechartVisjs: require('./prototype-views/timechart-visjs')
 };
+
+(function () {
+    // Establish the root object, `window` in the browser, or `global` on the server.
+    var root = this; 
+
+    // Create a reference to this
+    var _ = api;
+
+    var isNode = false;
+
+    // Export the Underscore object for **CommonJS**, with backwards-compatibility
+    // for the old `require()` API. If we're not in CommonJS, add `_` to the
+    // global object.
+    if (typeof module !== 'undefined' && module.exports) {
+            module.exports = _;
+            root._ = _;
+            isNode = true;
+    } else {
+            root._ = _;
+    }
+})();
